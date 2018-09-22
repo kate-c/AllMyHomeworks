0904b81cfe72d510ce95706dff5be7938f0a5f28
kate-c <enchirkova@edu.hse.ru>
2018-09-22 13:09:55 +0300

    NEW TEST

---
+++ 1sem/ConsoleApp1/TEST_TEST_TEST/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TEST_TEST_TEST
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! HELLO TEST TEST TEST");
+        }
+    }
+}
